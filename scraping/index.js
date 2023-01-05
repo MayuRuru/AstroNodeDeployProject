ea7ef220c6acbbde027d19b22ed58f1005bc4bc1
MayuRuru <aruizrull@protonmail.com>
2023-01-05 19:43:16 +0100

    commit

--- scraping/index.js
+++ scraping/index.js
@@ -19,30 +19,38 @@ async function scrape (url) {
 
 async function getBoardData() {
   // Using Cheerio to retrieve data for our API
+  const $ = await scrape(URLS.board)
+  const $rows = $('table tbody tr')
 
-  const $ = scrape(URLS.board)
+  const cleanText = text => text.replace(/\t|\n|\s:/g, '').replace(/.*:/g, '')
 
-  $('table tbody tr').each((index, el) => {
+  $rows.each((index, el) => {
     // console.log($(el).text())
-    const rawTeam = $(el).find('.fs-table-text_3').text()
-    const rawWins = $(el).find('.fs-table-text_4').text()
-    const rawLoses = $(el).find('.fs-table-text_5').text()
-    const rawScoredGoals = $(el).find('.fs-table-text_6').text()
-    const rawConcededGoals = $(el).find('.fs-table-text_7').text()
-    const rawYellowCards = $(el).find('.fs-table-text_8').text()
-    const rawRedCards = $(el).find('.fs-table-text_9').text()
 
-    console.log({
-      rawTeam,
-      rawWins
-    })
+    const $el = $(el)
+    const rawTeam = $el.find('.fs-table-text_3').text()
+    const rawWins = $el.find('.fs-table-text_4').text()
+    const rawLoses = $el.find('.fs-table-text_5').text()
+    const rawScoredGoals = $el.find('.fs-table-text_6').text()
+    const rawConcededGoals = $el.find('.fs-table-text_7').text()
+    const rawYellowCards = $el.find('.fs-table-text_8').text()
+    const rawRedCards = $el.find('.fs-table-text_9').text()
+
+    console.log(
+      cleanText(rawTeam),
+      cleanText(rawWins),
+      cleanText(rawLoses),
+      cleanText(rawScoredGoals),
+      cleanText(rawConcededGoals),
+      cleanText(rawYellowCards),
+      cleanText(rawRedCards)
+    )
   })
 }
 
 // Design API:
-
-const board = [
-  {
+/* 
+const board = [{
     team: 'Team 1',
     wins: 0,
     loses: 0,
@@ -50,5 +58,5 @@ const board = [
     concededGoals: 0,
     yellowCards: 0,
     redCards: 0
-  }
-]
+  }]
+ */
